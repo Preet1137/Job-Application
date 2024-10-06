3b52ba44807b558c0afd6abc95a17f37b9cdd99a
Preet Dhoble <preetdhoble1137@gmail.com>
2024-10-06 16:01:41 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,33 +2,50 @@ import './App.css';
 import { useEffect, useState } from 'react';
 
 const App = () => {
-  const [users, setUsers] = useState([]);
+  const [allValues, setValues] = useState({
+    userlist: [],
+    showLoader: false
+  });
 
-  useEffect(() => {
-    const fetchApi = async () => {
-      const api = 'https://gorest.co.in/public/v2/users';
+  const fetchApi = async () => {
+    setValues({ ...allValues, showLoader: true });
+    const api = 'https://gorest.co.in/public/v2/users';
+
+    try {
       const response = await fetch(api);
       const data = await response.json();
-      setUsers(data);
-    };
-
-    fetchApi();
-  }, []);
+      if (response.ok === true) {
+        setValues({ ...allValues, userlist: data, showLoader: false });
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
-    <div>
-  
-      <div className="cards-cont">
-        {users.map((user) => (
-          <div key={user.id} className="card">
-            <h2>{user.name}</h2>
-            <br />
-            <p>Email: {user.email}</p>
-            <p>Gender: {user.gender}</p>
-            <p>Status: {user.status}</p>
-          </div>
-        ))}
+    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+      <button onClick={fetchApi} className='btn btn-primary'>Fetch</button>
+      <br /><br />
+
+      {allValues.showLoader ? (
+        <div class="spinner-border text-success" role="status">
+        <span class="sr-only">Loading...</span>
       </div>
+      ) : (
+        <div className="cards-cont">
+          {allValues.userlist.map((user) => (
+            <div key={user.id} className="card">
+              <ul>
+                <h2>{user.name}</h2>
+                <br />
+                <p>Email: {user.email}</p>
+                <p>Gender: {user.gender}</p>
+                <p>Status: {user.status}</p>
+              </ul>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
